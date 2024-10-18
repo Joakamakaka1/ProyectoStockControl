f7395f742befd58be8604c110be89721bc4e4bd7
Joakamakaka <jcastrosalas03@gmail.com>
2024-10-18 19:03:31 +0200

    Clases Proveedor y User realizada

--- src/main/java/com/es/stockcontrol/model/Proveedor.java
+++ src/main/java/com/es/stockcontrol/model/Proveedor.java
@@ -1,4 +1,20 @@
 package com.es.stockcontrol.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table (name = "proveedor")
 public class Proveedor {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column (name = "nombre", length = 50, nullable = false)
+    private String nombre;
+    @Column (name = "direccion", nullable = false)
+    private String direccion;
+    @OneToMany(mappedBy = "proveedor")
+    List<Producto> productos; //Relacion de <OneToMany>
 }
--- src/main/java/com/es/stockcontrol/model/User.java
+++ src/main/java/com/es/stockcontrol/model/User.java
@@ -1,4 +1,37 @@
 package com.es.stockcontrol.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private String nombre_usuario;
+    @Column (name = "nombre", length = 20, nullable = false)
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(String nombre_usuario, String password) {
+        this.nombre_usuario = nombre_usuario;
+        this.password = password;
+    }
+
+    public String getNombre_usuario() {
+        return nombre_usuario;
+    }
+    public void setNombre_usuario(String nombre_usuario) {
+        this.nombre_usuario = nombre_usuario;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
