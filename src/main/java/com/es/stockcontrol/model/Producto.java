9b7e944fb7a6198364bfc6957fcdb3c6374c9a5c
PabloGonz <pablogonzalezsilva6@gmail.com>
2024-10-18 19:04:17 +0200

    Clase Producto creada

--- src/main/java/com/es/stockcontrol/model/Producto.java
+++ src/main/java/com/es/stockcontrol/model/Producto.java
@@ -1,4 +1,108 @@
 package com.es.stockcontrol.model;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.Cascade;
+
+import java.util.Date;
+
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(name = "categoria", nullable = false, length = 10)
+    private String categoria;
+    @Column(name = "nombre", nullable = false, length = 50)
+    private String nombre;
+    @Column(name = "descripcion")
+    private String descripcion;
+    @Column(name = "precio_sin_iva", nullable = false)
+    private Float precioSinIva;
+    @Column(name = "precio_con_iva", nullable = false)
+    private Float precioConIva;
+    @Column(name = "fecha_alta", nullable = false)
+    private Date fechaAlta;
+
+    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id-proveedor")
+    private Proveedor proveedor;
+
+    public Producto() {
+    }
+
+    public Producto(String id, String categoria, String nombre, String descripcion, Float precioSinIva, Float precioConIva, Date fechaAlta, Proveedor proveedor) {
+        this.id = id;
+        this.categoria = categoria;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precioSinIva = precioSinIva;
+        this.precioConIva = precioConIva;
+        this.fechaAlta = fechaAlta;
+        this.proveedor = proveedor;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Float getPrecioSinIva() {
+        return precioSinIva;
+    }
+
+    public void setPrecioSinIva(Float precioSinIva) {
+        this.precioSinIva = precioSinIva;
+    }
+
+    public Float getPrecioConIva() {
+        return precioConIva;
+    }
+
+    public void setPrecioConIva(Float precioConIva) {
+        this.precioConIva = precioConIva;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
 }
