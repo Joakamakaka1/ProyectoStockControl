9b4a2999c435a283187bc50627cea6fe24011ee3
Joakamakaka <jcastrosalas03@gmail.com>
2024-10-18 19:07:33 +0200

    Constructuores y get y set de Proveedor

--- src/main/java/com/es/stockcontrol/model/Proveedor.java
+++ src/main/java/com/es/stockcontrol/model/Proveedor.java
@@ -15,6 +15,49 @@ public class Proveedor {
     private String nombre;
     @Column (name = "direccion", nullable = false)
     private String direccion;
-    @OneToMany(mappedBy = "proveedor")
+    @OneToMany(mappedBy = "id_proveedor")
     List<Producto> productos; //Relacion de <OneToMany>
+
+
+    public Proveedor() {
+    }
+
+    public Proveedor(Long id, String nombre, String direccion, List<Producto> productos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.productos = productos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
 }
