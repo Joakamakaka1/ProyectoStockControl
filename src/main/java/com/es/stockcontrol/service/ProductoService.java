7824647455b4daf8341b9defb9066fcfa9d5162e
Joakamakaka <jcastrosalas03@gmail.com>
2024-10-22 20:34:19 +0200

    ProductoService terminado

--- src/main/java/com/es/stockcontrol/service/ProductoService.java
+++ src/main/java/com/es/stockcontrol/service/ProductoService.java
@@ -1,9 +1,11 @@
 package com.es.stockcontrol.service;
 
 import com.es.stockcontrol.model.Producto;
+import com.es.stockcontrol.model.Proveedor;
 import com.es.stockcontrol.repository.ProductoRepository;
 
 import java.util.Date;
+import java.util.List;
 
 public class ProductoService {
 
@@ -13,7 +15,7 @@ public class ProductoService {
         this.productoRepository = productoRepository;
     }
 
-    public Producto agregarProducto(String categoria, String nombre, Float precioSinIva, Float precioConIva, String proveedor) {
+    public Producto agregarProducto(String categoria, String nombre, Float precioSinIva, Float precioConIva, Proveedor proveedor) {
         if (categoria == null || categoria.length() < 3 || categoria.length() > 50) {
             return null;
         }
@@ -43,15 +45,51 @@ public class ProductoService {
     }
 
     // Generamos la ID del producto ya que no tiene la anotacion para hacerlo en este caso
-    private String generarId(String categoria, String nombre, String proveedor) {
+    public String generarId(String categoria, String nombre, Proveedor proveedor) {
         String categoriaAbreviada = categoria.length() >= 3 ? categoria.substring(0, 3) : categoria;
         String nombreAbreviado = nombre.length() >= 3 ? nombre.substring(0, 3) : nombre;
-        String proveedorAbreviado = proveedor.length() >= 3 ? proveedor.substring(0, 3) : proveedor;
+        String proveedorAbreviado = proveedor != null ? proveedor.getNombre().length() >= 3 ? proveedor.getNombre().substring(0, 3) : proveedor.getNombre() : "";
         return categoriaAbreviada + nombreAbreviado + proveedorAbreviado;
     }
 
-    private Float calcularPrecioConIva(Float precioSinIva) {
+    public Float calcularPrecioConIva(Float precioSinIva) {
         final float IVA = 0.21f;
         return precioSinIva * (1 + IVA);
+    }
+
+    public Producto getProducto(String id) {
+        return productoRepository.buscarPorId(id);
+    }
+
+    public Producto actualizarProducto(String id, String categoria, String nombre, Float precioSinIva, Float precioConIva, Proveedor proveedor) {
+        Producto producto = productoRepository.buscarPorId(id);
+        if (producto == null) {
+            return null;
+        }
+        if (categoria != null && !categoria.isEmpty()) {
+            producto.setCategoria(categoria);
+        }
+        if (nombre != null && !nombre.isEmpty()) {
+            producto.setNombre(nombre);
+        }
+        if (proveedor != null) {
+            producto.setProveedor(proveedor);
+        }
+        if (precioSinIva != null) {
+            producto.setPrecioSinIva(precioSinIva);
+        }
+        if (precioConIva != null) {
+            producto.setPrecioConIva(precioConIva);
+        }
+        productoRepository.actualizar(producto);
+        return producto;
+    }
+
+    public void eliminarProducto(String id) {
+        Producto producto = productoRepository.buscarPorId(id);
+        if (producto == null) {
+            return;
+        }
+        productoRepository.eliminar(producto);
     }
 }
