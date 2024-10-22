6cb82546d4ca02ca7525da11e78a87adf647ccc8
PabloGonz <pablogonzalezsilva6@gmail.com>
2024-10-22 20:05:14 +0200

    Proveedor Service Terminado User Service Eliminado  (no  es necesario)

--- src/main/java/com/es/stockcontrol/controller/impl/UserController.java
+++ src/main/java/com/es/stockcontrol/controller/impl/UserController.java
@@ -4,14 +4,14 @@ package com.es.stockcontrol.controller.impl;
 import com.es.stockcontrol.controller.api.UserControllerAPI;
 import com.es.stockcontrol.model.RespuestaHTTP;
 import com.es.stockcontrol.model.User;
-import com.es.stockcontrol.service.UserService;
+import com.es.stockcontrol.repository.UserRepository;
 
 public class UserController implements UserControllerAPI {
 
-    private UserService userService;
+    private UserRepository userRepository;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
--- src/main/java/com/es/stockcontrol/service/ProveedorService.java
+++ src/main/java/com/es/stockcontrol/service/ProveedorService.java
@@ -1,5 +1,6 @@
 package com.es.stockcontrol.service;
 
+import com.es.stockcontrol.model.Proveedor;
 import com.es.stockcontrol.repository.ProveedorRepository;
 
 public class ProveedorService {
@@ -9,4 +10,52 @@ public class ProveedorService {
     public ProveedorService(ProveedorRepository proveedorRepository) {
         this.proveedorRepository = proveedorRepository;
     }
+
+    public Proveedor agregarProveedor(String nombre, String direccion) {
+        if (nombre == null || nombre.isEmpty() || direccion == null || direccion.isEmpty()) {
+            return null;
+        }
+        if (proveedorRepository.buscarTodos().stream().anyMatch(p -> p.getNombre().equals(nombre))) {
+            return null;
+        }
+        if (nombre.length()<0 || nombre.length()>50) {
+            return null;
+        }
+
+        Proveedor proveedor = new Proveedor();
+        proveedor.setNombre(nombre);
+        proveedor.setDireccion(direccion);
+        proveedorRepository.guardar(proveedor);
+        return proveedor;
+    }
+
+    public Proveedor actualizarProveedor(Long id, String nombre, String direccion) {
+        Proveedor proveedor = proveedorRepository.buscarPorId(id);
+        if (proveedor==null) {
+            return null;
+        }
+        if (nombre != null && !nombre.isEmpty()) {
+            if (nombre.length()<0 || nombre.length()>50) {
+                return null;
+            }
+            proveedor.setNombre(nombre);
+        }
+        if (direccion != null && !direccion.isEmpty()) {
+            if (direccion.length()<0 || direccion.length()>50) {
+                return null;
+            }
+            proveedor.setDireccion(direccion);
+        }
+        proveedorRepository.actualizar(proveedor);
+        return proveedor;
+    }
+
+    public void eliminarProveedor(Long id) {
+        Proveedor proveedor = proveedorRepository.buscarPorId(id);
+        if (proveedor != null) {
+            proveedorRepository.eliminar(proveedor);
+        }
+    }
+
+
 }
